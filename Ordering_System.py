b7cbec4de8582da267d5b83a28f1899b00ed5103
Paul Nerie <pa6308191@gmail.com>
2026-01-09 19:40:26 +0800

    Created simple window widget to make sure customtkinter works.

--- Ordering_System.py
+++ Ordering_System.py
@@ -1,4 +1,15 @@
 import customtkinter as ctk
-from tkinter import ttk, messagebox
+from tkinter import messagebox
 from PIL import Image
 
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+
+window = ctk.CTk()
+window.geometry("400x300")
+window.title("CustomTkinter Test")
+
+label = ctk.CTkLabel(window, text="Hello CustomTkinter!")
+label.pack(pady=20)
+
+window.mainloop()
