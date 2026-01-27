ff184d66b8bbe3a6949c6f324c2fd4c67553b993
Paul Nerie <pa6308191@gmail.com>
2026-01-27 18:52:24 +0800

    Make an apprpriate logo icon for the system.

--- Ordering_System.py
+++ Ordering_System.py
@@ -5,12 +5,15 @@ from PIL import Image, ImageTk
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
 
-login_window = ctk.CTk()
-login_window.geometry("400x300") # Window size
-login_window.title("Kape'Bahay Ordering System - Login") # Window Title
-login_window.iconbitmap("windowLogo.ico") # Window Icon
+def create_login_window():
+    login_window = ctk.CTk()
+    login_window.geometry("400x300") # Window size
+    login_window.title("Kape'Bahay Ordering System - Login") # Window Title
+    login_window.iconbitmap("logo file path here") # the icon file must be in .ico format and must be placed in the same folder as the system
 
-label = ctk.CTkLabel(login_window, text="Hello CustomTkinter!")
-label.pack(pady=20)
+    label = ctk.CTkLabel(login_window, text="Hello CustomTkinter!")
+    label.pack(pady=20)
 
-login_window.mainloop()
+loginWindow = create_login_window()
+
+loginWindow.mainloop()
