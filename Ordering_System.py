23da8d605e1682b1367364ef3dd17c54c19b55c2
Paul Nerie <pa6308191@gmail.com>
2026-01-27 19:17:51 +0800

    Created 2 frames for the positions of the widgets to be placed in the loginWindow

--- Ordering_System.py
+++ Ordering_System.py
@@ -5,19 +5,29 @@ from PIL import Image, ImageTk
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
 
-# Use Camel Case for naming conventions of variables and functions, so that it is easier to read.
+# IMPORTANT NOTES: Basahin para maintindihan ko mga code ninyo.
+# You can use ChatGPT how to use CustomTkinter it's similar to Tkinter but with more modern looks.
+# Use Camel Case for naming conventions of variables and functions, so that it is easier to read. 
+# Example: firstLetterIsCapitalCaseExceptforTheFirstWord
 # NOTICE: Whenever you create a new window, make sure its placed inside a "def" function and name them properly to avoid confusion.
 # USE "create_[window name]_window()" format when naming the function.
 # Example: create_login_window(), create_main_window(), create_order_window(), etc.
 
 def create_login_window():
     login_window = ctk.CTk()
-    login_window.geometry("800x700") # Window size
+    login_window.geometry("900x700") # Window size
     login_window.title("Kape'Bahay Ordering System - Login") # Window Title
     #login_window.iconbitmap("logo file path here") # the icon file must be in .ico format and must be placed in the same folder as the system
 
-    label = ctk.CTkLabel(login_window, text="Hello CustomTkinter!")
-    label.pack(pady=20)
+    # Left Frame
+    left_frame = ctk.CTkFrame(login_window, fg_color="#2A8F5C")
+    left_frame.pack(side="left",fill="both", expand=True)
+
+
+    # Right Frame
+    right_frame = ctk.CTkFrame(login_window, fg_color="#3032AA")
+    right_frame.pack(side="right", fill="both", expand=True)
+
 
     return login_window
 
